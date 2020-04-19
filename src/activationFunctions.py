f05181ea7e5999142cd04fc4a0975814a80e14aa
perfan <56326009+perfan@users.noreply.github.com>
2020-04-18 22:08:14 -0400

    Added Relu activation function, backward Relu, and backward Sigmoid

--- src/activationFunctions.py
+++ src/activationFunctions.py
@@ -1,18 +1,80 @@
 
-"""
-Compute the sigmoid of z
-
-Arguments:
-z -- A scalar or numpy array of any size.
-
-Return:
-s -- sigmoid(z)
-"""
-
 import numpy as np 
  
-def sigmoid(z):
+def sigmoid(Z):
+    """
+    Compute the sigmoid of z
 
-    s = 1/(1+np.exp(-z))
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+
+    A = 1/(1+np.exp(-Z))
+    cache = Z
+
+    return A, cache
+
+def relu(Z):
+    """
+    Implement the RELU function.
+
+    Arguments:
+    Z -- Output of the linear layer, of any shape
+
+    Returns:
+    A -- Post-activation parameter, of the same shape as Z
+    cache -- a python dictionary containing "A" ; stored for computing the backward pass efficiently
+    """
     
-    return s
+    A = np.maximum(0,Z)
+    
+    assert(A.shape == Z.shape)
+    
+    cache = Z 
+    return A, cache
+
+def relu_backward(dA, cache):
+    """
+    Implement the backward propagation for a single RELU unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+    
+    Z = cache
+    dZ = np.array(dA, copy=True) # just converting dz to a correct object.
+    
+    # When z <= 0, you should set dz to 0 as well. 
+    dZ[Z <= 0] = 0
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
+
+def sigmoid_backward(dA, cache):
+    """
+    Implement the backward propagation for a single SIGMOID unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+    
+    Z = cache
+    
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
